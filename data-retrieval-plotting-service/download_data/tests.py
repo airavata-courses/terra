e6d858645225918900c44b7d62e906c045fcd380
GowthamChowta <gautigadu091@gmail.com>
2022-02-07 13:22:39 -0500

    unit test cases for Django app #22

--- data-retrieval-plotting-service/download_data/tests.py
+++ data-retrieval-plotting-service/download_data/tests.py
@@ -1,3 +1,19 @@
-from django.test import TestCase
-
 # Create your tests here.
+from fileinput import filename
+from django.test import TestCase
+from .models import ImagePath
+from .views import DownloadDataViewSet
+from django.test import Client
+# Create your tests here.
+
+class ImagePathTestCase(TestCase): 
+    
+    def setUp(self):
+        obj1 = ImagePath(filename='f', url='url')
+        obj1.save()
+    
+    def test_object_is_created(self):
+        """Image path object creation is successful"""
+        obj = ImagePath.objects.get(filename="f")                
+        self.assertEqual(obj.url, 'url')
+
