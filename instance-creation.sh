a0b48cddf87101a138e0de057a4a2c161beb1a59
GowthamChowta <gautigadu091@gmail.com>
2022-03-29 17:10:48 -0400

    push the docker changes

--- instance-creation.sh
+++ instance-creation.sh
@@ -41,6 +41,7 @@ ansible-playbook -i hosts master.yml &&
 sleep 30 &&
 ansible-playbook -i hosts workers.yml &&
 ssh ubuntu@$IP &&
+sudo su &&
 git clone https://github.com/airavata-courses/terra.git &&
 cd terra &&
 git checkout ansible-kub-setup &&
@@ -55,3 +56,4 @@ kubectl apply -f api-gateway.yaml &&
 kubectl apply -f weatherForecast.yaml &&
 kubectl apply -f user-managment.yaml && 
 kubectl apply -f userinterface.yaml &&
+kubectl get pods
