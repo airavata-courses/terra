58a84d82b7d72a05dd0fee67fb6ce06a33a7827e
GowthamChowta <gautigadu091@gmail.com>
2022-03-29 12:31:05 -0400

    Adding kubernetes setup files

--- instance-creation.sh
+++ instance-creation.sh
@@ -10,12 +10,15 @@ sudo chmod +x terraform &&
 sudo cp terraform /usr/local/bin/ &&
 sudo mv terraform /usr/bin/ &&
 rm -rf terraform_0.14.4_linux_amd64.zip &&
+# Change the directory
+cd jetstream_kubespray &&
+# Export cluser name
+export CLUSTER=terraclu &&
+cp -r inventory/kubejetstream inventory/$CLUSTER &&
+cd inventory/$CLUSTER &&
+# Update the IP address you have provided
+sed -i '/k8s_master_fips/c\k8s_master_fips =["'$IP'"]'  cluster.tfvars &&
 
-export CLUSTER=terraclu
-cp -r inventory/kubejetstream inventory/$CLUSTER
-cd inventory/$CLUSTER
-sed -i '/k8s_master_fips/c\k8s_master_fips "=['$IP']"'  cluster.tfvars
-
-bash terraform_init.sh
-bash terraform_apply.sh
+bash terraform_init.sh &&
+bash terraform_apply.sh &&
 
