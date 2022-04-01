a2ceecf4a293a15a9796f09d8f7f16516b56897f
Joshi, Himanshu <hsjoshi@iu.edu>
2022-04-01 12:20:11 -0400

    CI setup Testing

--- user.session.management/src/main/java/team/terra/user/session/management/controller/UserController.java
+++ user.session.management/src/main/java/team/terra/user/session/management/controller/UserController.java
@@ -49,7 +49,7 @@ public class UserController {
 	 */
 	@PostMapping("/data")
 	public UserResponseModel userData(@RequestBody UserRequestModel userRequestModel) {
-		logger.info("controller : user/data Test CI-CD5");
+		logger.info("controller : user/data Test CI-CD6");
 		return userService.userLogService(userRequestModel); 
 	}
 	
