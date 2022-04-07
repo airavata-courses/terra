85b388b791d3539092385887618ae7877b0e3d81
Joshi, Himanshu <hsjoshi@iu.edu>
2022-04-06 21:47:21 -0400

    SQL Search output change

--- user.session.management/DB_docker/generate_table.sql
+++ user.session.management/DB_docker/generate_table.sql
@@ -2,6 +2,12 @@ CREATE DATABASE IF NOT EXISTS team_terra;
 
 USE team_terra; 
 
+DROP TABLE IF EXISTS user_entity;
+DROP TABLE IF EXISTS user_log_entity;
+DROP TABLE IF EXISTS user_activity_entity;
+DROP TABLE IF EXISTS user_register_entity
+
+
 CREATE TABLE IF NOT EXISTS user_entity (
     user_id   INT			PRIMARY KEY,
     name      VARCHAR(50),
@@ -20,9 +26,9 @@ CREATE TABLE IF NOT EXISTS user_activity_entity (
 	user_activity_id 	INT		PRIMARY KEY,
 	user_id				INT,
 	token_id 			VARCHAR(100),
-	type_of_search 		VARCHAR(20),
+	type_of_search 		VARCHAR(100),
 	search_output		VARCHAR(500),
-	search_param		VARCHAR(80)
+	search_param		VARCHAR(200)
 );
 
 CREATE TABLE IF NOT EXISTS user_register_entity (
